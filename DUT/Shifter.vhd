849a7be5204911177ab779faeef1c6f1ca577b3e
YarinOz <oziely@post.bgu.ac.il>
2024-05-14 13:03:03 +0300

    shifter compiles

--- DUT/Shifter.vhd
+++ DUT/Shifter.vhd
@@ -2,7 +2,8 @@ LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 --------------------------------------------------------
 ENTITY Shifter IS
-    GENERIC (n: INTEGER := 8);
+    GENERIC (n: INTEGER := 8; -- number of bits
+			 k: INTEGER := 3); -- log2(n)
 	PORT (x, y : IN std_logic_vector(n-1 DOWNTO 0); -- shift y x times
           dir: IN std_logic;	-- 0: left, 1: right
 		  cout: OUT std_logic;
@@ -15,6 +16,7 @@ ARCHITECTURE BarShift OF Shifter IS
 	TYPE matrix is ARRAY (k-1 DOWNTO 0) OF vector; -- log2n x n
 	SIGNAL STEP: matrix;
 	SIGNAL carry: std_logic_vector(k-1 DOWNTO 0);
+	SIGNAL zero_vector : std_logic_vector(k-1 DOWNTO 0) := (OTHERS => '0');
 BEGIN
 	first: for i in 0 to n-1 generate  -- initialize
 		STEP(0)(i) <= y(i) WHEN dir = '0' ELSE y(n-1-i);
@@ -37,5 +39,5 @@ BEGIN
 		res(i) <= STEP(k-1)(i) WHEN dir = '0' ELSE STEP(k-1)(n-1-i);
 	END generate final;
 
-	cout <= '0' WHEN carry = (OTHERS => '0') ELSE '1';
+	cout <= '0' WHEN carry = zero_vector ELSE '1';
 END BarShift;
